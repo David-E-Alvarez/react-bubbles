bf48b8dc8724f1444a3d4125589a96ab905373fc
david <davidealvarez1@gmail.com>
2020-03-27 08:53:59 -0700

    stage 1 completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4"
+})
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  axiosWithAuth()
+            .post("/api/login", credentials)
+            .then(res => {
+                console.log("res.data.token: ", res.data.payload);
+                console.log("res: ", res)
+                localStorage.setItem('token', res.data.payload);
+                // if(token){
+                //     setLogin(true);
+                // }
+                props.history.push('/bubbles');
+              })
+              .catch(err => console.log(err.response));
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
